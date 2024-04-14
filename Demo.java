dc76721924444c04df8f0fbed1553bcc14c3f5b3
Viiiiikaaaaa <116728003+Viiiiikaaaaa@users.noreply.github.com>
2024-04-14 14:44:44 +0300

    Demo.java

--- Demo.java
+++ Demo.java
@@ -1,4 +1,11 @@
 public class Demo {
+    public static double calculateTotalWeight(Box<?>... boxes){
+        double totalWeight = 0;
+        for (Box<?> box:boxes){
+            totalWeight += box.calculateTotalWeight();
+        }
+        return totalWeight;
+    }
     public static void main(String[] args) {
         Apple apple = new Apple(30);
         Persimmon persimmon = new Persimmon(24);
@@ -6,6 +13,7 @@ public class Demo {
         Box<Apple> appleBox2 = new Box<>(apple, 5);
         Box<Persimmon> persimmonBox1 = new Box<>(persimmon, 8);
         Box<Persimmon> persimmonBox2 = new Box<>(persimmon, 7);
-        System.out.println("Общая масса нескольких коробок: "+ (appleBox1.calculateTotalWeight()+ appleBox2.calculateTotalWeight()+ persimmonBox1.calculateTotalWeight()+ persimmonBox2.calculateTotalWeight()));
+        double totalWeight = calculateTotalWeight(appleBox1,appleBox2,persimmonBox2,persimmonBox1);
+        System.out.println("Общая масса нескольких коробок: "+ totalWeight);
     }
 }
